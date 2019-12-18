9313a47fbfc50e189d9f6c6a2e69c7f76bbf8276
juangom73 <juangom73@gmail.com>
2019-12-18 17:07:16 -0500

    Add Error Boundry

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,53 +1,59 @@
-import React, { Component } from 'react';
-import CardList from '../components/CardList';
-import SearchBox from '../components/SearchBox';
-import Scroll from '../components/Scroll';
-import './App.css';
+import React, { Component } from "react";
+import CardList from "../components/CardList";
+import SearchBox from "../components/SearchBox";
+import Scroll from "../components/Scroll";
+import ErrorBoundry from "../components/ErrorBoundry";
+import "./App.css";
 
 class App extends Component {
   //We need to add this to add a state
   //State use to live in the app component
   constructor() {
-    super()
+    super();
     this.state = {
       robots: [],
-      searchfield: ''
-    }
+      searchfield: ""
+    };
   }
 
   componentDidMount() {
     //fetch is an object that comes with the browser that is going to allow us to do api calls
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response=> response.json())
-      .then(users => {this.setState({ robots: users})});
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users => {
+        this.setState({ robots: users });
+      });
   }
 
   //everytime that onChange changes we got an event
   //You should use arrow functions everytime that you want to add a method into a component
   // so you can have the right value of "this" refers to this context
-  onSearchChange = (event) => {
+  onSearchChange = event => {
     //Everytime that we render the state the "render" will be triger again, because the compoent is going to be re-render
-    this.setState({ searchfield: event.target.value })
-  }
+    this.setState({ searchfield: event.target.value });
+  };
 
   render() {
     const { robots, searchfield } = this.state;
-    const filteredRobots = robots.filter(robot =>{
+    const filteredRobots = robots.filter(robot => {
       return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-    })
+    });
 
     //We check if we get the information yet.
-    return !robots.length ?
-      <h1>Loading</h1> :
-      (
-        <div className='tc'>
-          <h1 className='f1'>RoboFriends</h1>
-          <SearchBox searchChange={this.onSearchChange}/>
-          <Scroll>
+    // ErrorBoundry shows a nice error to the end customer
+    return !robots.length ? (
+      <h1>Loading</h1>
+    ) : (
+      <div className="tc">
+        <h1 className="f1">RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <Scroll>
+          <ErrorBoundry>
             <CardList robots={filteredRobots} />
-          </Scroll>
-        </div>
-      );
+          </ErrorBoundry>
+        </Scroll>
+      </div>
+    );
   }
 }
 
